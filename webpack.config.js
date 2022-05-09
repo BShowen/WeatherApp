78cfd884af86d8e4400a5b5e129aa92bf63df555
Bradley Showen <bshowen@live.seminolestate.edu>
2022-05-09 17:33:35 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Weather",
+      meta: {
+        viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
+        charset: "UTF-8",
+      },
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
